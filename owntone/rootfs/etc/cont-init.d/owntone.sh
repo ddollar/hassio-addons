0eeff9a2e0155ab5358601cbe973203ba1144bb2
David Dollar <david@dollar.io>
2021-08-27 08:42:10 -0400

    use owntone from johnpdowling

--- owntone/rootfs/etc/cont-init.d/owntone.sh
+++ owntone/rootfs/etc/cont-init.d/owntone.sh
@@ -1,22 +1,21 @@
 #!/usr/bin/with-contenv bashio
-# ==============================================================================
-# Ulrar Hass.io Add-ons: owntone server
-# Configures the owntone Server
-# ==============================================================================
 
-if ! bashio::fs.directory_exists '/share/owntone/cache'; then
+#Start it up
+
+if ! bashio::fs.directory_exists '/config/owntone/cache'; then
     bashio::log.debug 'Creating cache folder...'
-    mkdir -p /share/owntone/cache
+    mkdir -p /config/owntone/cache
 fi
 
-if ! bashio::fs.file_exists '/share/owntone/owntone.conf'; then
+if ! bashio::fs.file_exists '/config/owntone/owntone.conf'; then
     bashio::log.debug 'Copying default conf file...'
-    cp /usr/local/etc/owntone.conf /share/owntone/owntone.conf
+    cp /usr/local/etc/owntone.conf /config/owntone/owntone.conf
 fi
 
-if ! bashio::fs.directory_exists '/share/owntone/music'; then
-    bashio::log.debug 'Creating music folder...'
-    mkdir -p /share/owntone/music
+if ! bashio::fs.directory_exists '/config/owntone/music'; then
+bashio::log.debug 'Creating music folder...'
+    mkdir -p /config/owntone/music
     bashio::log.debug 'Creating HA fifo file...'
-    mkfifo -m 666 /share/owntone/music/HomeAssistantAnnounce
+    mkfifo -m 666 /config/owntone/music/HomeAssistantAnnounce
 fi
+
